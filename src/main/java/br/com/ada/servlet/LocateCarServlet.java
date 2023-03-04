cd1e16cb59e58d672daea03278203aafe27d9aa1
hilariohenrique <hilariohenr@hotmail.com>
2023-03-03 23:53:12 -0300

    --

---
+++ src/main/java/br/com/ada/servlet/LocateCarServlet.java
@@ -0,0 +1,22 @@
+package br.com.ada.servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+
+@WebServlet("/locateCarServlet")
+public class LocateCarServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        PrintWriter output = resp.getWriter();
+        output.println("<h1>Minha Servlet!</h1>");
+
+    }
+}
